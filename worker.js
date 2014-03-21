114a2f440721c5ba76b827c90efb584f7ba6e80a
Adam Duncan <adam.duncan@clock.co.uk>
2014-03-21 22:20:28 +0000

    Prevent webhook from firing mulitple times on subsequent executions

--- worker.js
+++ worker.js
@@ -5,10 +5,10 @@ module.exports = {
   // run for each job
   init: function (config, job, context, cb) {
     var hooks = utils.makeWebHooks(config || [], job)
-    console.log('init wh', config, hooks)
     cb(null, {
       listen: function (io, context) {
-        io.on('job.status.tested', function (id, data) {
+        function onTested(id, data) {
+          io.removeListener('job.status.tested', onTested)
           hooks.forEach(function (hook) {
             context.comment('Firing webhook ' + hook.title)
             try {
@@ -19,7 +19,8 @@ module.exports = {
               return
             }
           })
-        })
+        }
+        io.on('job.status.tested', onTested)
       }
     })
   },
