d2b859a04c876e30d2591b9c7de8683ae560666d
ProZachJ <prozachj@gmail.com>
2014-08-09 19:22:13 -0500

    Refactor hook callbacks

--- worker.js
+++ worker.js
@@ -1,6 +1,37 @@
 
 var utils = require('./lib/utils')
 
+function onTested (id, data){
+  io.removeListener('job.status.tested', onTested)
+  hooks.forEach(function (hook) {
+    context.comment('Firing webhook ' + hook.title)
+    try {
+      var payload = hook.prepare(data, job)
+      io.emit('plugin.webhooks.fire', hook.url, hook.secret, payload)
+    } catch (e) {
+      context.comment('Failed to prepare webhook payload: ' + e.message);
+      return
+    }
+  })
+}
+
+function onDeployed (id, data){
+  io.removeListener('job.status.deployed', onDeployed)
+  hooks.forEach(function (hook) {
+    context.comment('Firing webhook ' + hook.title)
+    try {
+      var payload = hook.prepare(data, job)
+      payload['deploy_exitcode'] = (data.exitCode === 0) ? 0 : 1
+      console.log(data);
+      io.emit('plugin.webhooks.fire', hook.url, hook.secret, payload)
+    } catch (e) {
+      context.comment('Failed to prepare webhook payload: ' + e.message);
+      return
+    }
+  })
+}
+
+
 module.exports = {
   // run for each job
   init: function (config, job, context, cb) {
@@ -8,32 +39,8 @@ module.exports = {
     console.log('init wh', config, hooks)
     cb(null, {
       listen: function (io, context) {
-        io.on('job.status.tested', function (id, data) {
-          hooks.forEach(function (hook) {
-            context.comment('Firing webhook ' + hook.title)
-            try {
-              var payload = hook.prepare(data, job)
-              io.emit('plugin.webhooks.fire', hook.url, hook.secret, payload)
-            } catch (e) {
-              context.comment('Failed to prepare webhook payload: ' + e.message);
-              return
-            }
-          })
-        })
-        io.on('job.status.deployed', function (id, data){
-          hooks.forEach(function (hook) {
-            context.comment('Firing webhook ' + hook.title)
-            try {
-              var payload = hook.prepare(data, job)
-              payload['deploy_exitcode'] = (data.exitCode === 0) ? 0 : 1
-              console.log(data);
-              io.emit('plugin.webhooks.fire', hook.url, hook.secret, payload)
-            } catch (e) {
-              context.comment('Failed to prepare webhook payload: ' + e.message);
-              return
-            }
-          })
-        })
+        io.on('job.status.tested', onTested);
+        io.on('job.status.deployed', onDeployed)
       }
     })
   },
