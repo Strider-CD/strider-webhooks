f9d245ddd05706a3a5ce353d78863c3bb0da6b4b
Zach Jones <zach.jones@whitehatsec.com>
2014-08-09 19:52:41 -0500

    remove unneeded function

--- worker.js
+++ worker.js
@@ -1,23 +1,6 @@
 
 var utils = require('./lib/utils')
 
-
-
-function onDeployed (id, data){
-  io.removeListener('job.status.deployed', onDeployed)
-  hooks.forEach(function (hook) {
-    context.comment('Firing Deploy webhook ' + hook.title)
-    try {
-      var payload = hook.prepare(data, job)
-      payload['deploy_exitcode'] = (data.exitCode === 0) ? 0 : 1
-      io.emit('plugin.webhooks.fire', hook.url, hook.secret, payload)
-    } catch (e) {
-      context.comment('Failed to prepare webhook payload: ' + e.message);
-    }
-  })
-}
-
-
 module.exports = {
   // run for each job
   init: function (config, job, context, cb) {
